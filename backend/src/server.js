c41bb63a930a4909ec78ed1a93a457678fcc404f
Saransh rawat <Saranshrawat166@gmail.com>
2026-01-02 20:54:35 +0530

    backen setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
 
-// Routes
+// Routes are here
 app.use("/api/search", searchRoutes);
 app.use("/api/visited", visitedRoutes);
 
