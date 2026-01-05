dfab76b84a52c071c53bca135b4af5cdaa612afc
Saransh rawat <Saranshrawat166@gmail.com>
2026-01-05 19:55:35 +0530

    added auth0

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,12 +7,25 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import store from './redux/store/Store.js';
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from "@auth0/auth0-react";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <Auth0Provider
+  domain={process.env.REACT_APP_AUTH0_DOMAIN}
+  clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+    audience: process.env.REACT_APP_AUTH0_AUDIENCE
+  }}
+>
+
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
+
+    </Auth0Provider>
   </StrictMode>,
 )
